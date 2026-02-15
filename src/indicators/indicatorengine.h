285fcf4f3c3642489dd031bc20a83ce4b272f7e5
Alexey Gostyunin <bortone52@yandex.ru>
2026-02-15 13:51:27 +0300

    Add code to indicatorengine.h

--- src/indicators/indicatorengine.h
+++ src/indicators/indicatorengine.h
@@ -1,11 +1,24 @@
-#ifndef INDICATORENGINE_H
-#define INDICATORENGINE_H
-
+#pragma once
+#include "indicatortypes.h"
+#include "src/core/candleseries.h"
+#include <QSet>
 
 class IndicatorEngine
 {
+    QSet<IndicatorId> enabled_;
+    QVector<IndicatorLine> lines_;
+
 public:
     IndicatorEngine();
+
+    void setEnabled(IndicatorId id, bool on);
+    void isEnabled(Indicator Id) const;
+
+    void rebuild(const CandleSeries& series);
+
+    QVector<IndicatorLine> overlayLines() const;
+
+private:
+    static QVector<double> calcEma(const QList<Candle>& candles, int period);
 };
 
-#endif // INDICATORENGINE_H
