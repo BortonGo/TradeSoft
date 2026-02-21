56ad61fe754d608aa86e465c246c30a989dda8ff
Alexey Gostyunin <bortone52@yandex.ru>
2026-02-21 21:17:15 +0300

    Add code to traderecord.h

--- src/domain/trade/traderecord.h
+++ src/domain/trade/traderecord.h
@@ -1,4 +1,25 @@
-#ifndef TRADERECORD_H
-#define TRADERECORD_H
+#pragma once
+#include <QString>
+#include <QDateTime>
 
-#endif // TRADERECORD_H
+enum class TradeSide {
+    Buy,
+    Sell
+};
+
+enum class TradeStatus {
+    Open,
+    Closed,
+    PartiallyFilled,
+    Rejected
+};
+
+struct TradeRecord {
+    QDateTime time;
+    QString symbol;
+    TradeSide side = TradeSide::Buy;
+    double qty = 0.0;
+    double price = 0.0;
+    double fee = 0.0;
+    TradeStatus status = TradeStatus::Open;
+};
