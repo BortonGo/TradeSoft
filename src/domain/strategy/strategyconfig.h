37267acc98d908e989d0218c63bc1ac4f9b4fa75
Alexey Gostyunin <bortone52@yandex.ru>
2026-02-22 18:00:37 +0300

    Add code to strategyconfig

---
+++ src/domain/strategy/strategyconfig.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <QString>
+#include "core/timeframe.h"
+
+enum class RiskMode {
+    FixedUSDT,
+    PercentEquity
+};
+
+struct RiskSettings {
+    RiskMode mode = RiskMode::FixedUSDT;
+    double riskPct = 0.0;
+    double maxPosUsdt = 0.0;
+    int leverage = 1;
+    bool allowLong = true;
+    bool allowShort = true;
+    double feePct = 0.0;
+    int slippageBps = 0;
+};
+
+struct StrategySelection {
+    QString name;
+    Timeframe tf;
+};
+
+struct StrategyConfig {
+    StrategySelection strategy;
+    RiskSettings risk;
+    QString accountType;
+    QString account;
+};
+
