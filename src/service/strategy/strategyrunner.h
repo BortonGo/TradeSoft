43da887917e82e0cb4373ba7fea7632ebd46b10d
Alexey Gostyunin <bortone52@yandex.ru>
2026-03-03 14:32:27 +0300

    Write code to strategyrunner.h

--- src/service/strategy/strategyrunner.h
+++ src/service/strategy/strategyrunner.h
@@ -1,7 +1,37 @@
 #pragma once
+#include <memory>
+#include <QObject>
 
-class StrategyRunner
-{
+#include "service/marketdataservice.h"
+#include "domain/strategy/istrategy.h"
+#include "domain/strategy/strategysignal.h"
+
+class StrategyRunner final : public QObject {
+    Q_OBJECT
+
+    MarketDataService* mds_ = nullptr;
+    std::unique_ptr<IStrategy> strategy_;
+
+    StrategyContext ctx_;
+    bool running_ = false;
+
 public:
-    StrategyRunner();
+    explicit StrategyRunner(MarketDataService* mds, QObject* parent = nullptr);
+
+    void setStrategy(std::unique_ptr<IStrategy> s);
+    bool isRunning() const { return running_; }
+
+public slots:
+    void start(const QString& symbolId, Timeframe tf);
+    void stop();
+
+signals:
+    void signal_started();
+    void signal_stopped();
+    void signal_signalGenerated(StrategySignal s);
+
+private slots:
+    void onSeriesLoaded(std::shared_ptr<CandleSeries> series);
+    void onCandleClosed(Candle c);
+    void onCandleUpdate(Candle c);
 };
