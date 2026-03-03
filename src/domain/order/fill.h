35af86d939ab344197324c75014ee8d5bc7396a3
Alexey Gostyunin <bortone52@yandex.ru>
2026-03-03 15:53:04 +0300

    Write min.workable code to fill.h

--- src/domain/order/fill.h
+++ src/domain/order/fill.h
@@ -1,4 +1,16 @@
-#ifndef FILL_H
-#define FILL_H
+#pragma once
+#include <QString>
+#include <QDateTime>
+#include "domain/trade/traderecord.h"
 
-#endif // FILL_H
+struct Fill {
+    QDateTime time;
+    QString symbol;
+    TradeSide side = TradeSide::Buy;
+
+    double qty = 0.0;
+    double price = 0.0;
+
+    double fee = 0.0;
+    bool reduceOnly = false;
+};
