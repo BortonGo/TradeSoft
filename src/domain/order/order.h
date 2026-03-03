697a88baf18b3ef31c530599623de2a4c67b6254
Alexey Gostyunin <bortone52@yandex.ru>
2026-03-03 15:51:51 +0300

    Write min.workable code to order.h

--- src/domain/order/order.h
+++ src/domain/order/order.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <QString>
+#include "domain/trade/traderecord.h"
+#include <cstdint>
+
+enum class OrderType {
+    Market,
+    Limit
+};
+
+struct Order {
+    QString symbol;
+    TradeSide side = TradeSide::Buy;
+    OrderType type = OrderType::Market;
+
+    double qty = 0.0;
+    double limitPrice = 0.0;
+    bool reduceOnly = false;
+
+    int64_t clientOrderId = 0;
+};
