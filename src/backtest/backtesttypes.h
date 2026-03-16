590f28fb1227384fe26859eebc05eabdc6a529f3
Alexey Gostyunin <bortone52@yandex.ru>
2026-03-16 10:53:24 +0300

    Edit backtesttypes.h

--- src/backtest/backtesttypes.h
+++ src/backtest/backtesttypes.h
@@ -1,4 +1,56 @@
-#ifndef BACKTESTTYPES_H
-#define BACKTESTTYPES_H
+#pragma once
+#include <QString>
+#include <QDateTime>
+#include <vector>
 
-#endif // BACKTESTTYPES_H
+#include "domain/risk/riskconfig.h"
+#include "core/timeframe.h"
+
+enum class BacktestState {
+    Idle,
+    LoadingHistory,
+    Running,
+    Completed,
+    Cancelled,
+    Failed
+};
+
+enum class GraphType {
+    None,
+    EquityCurve,
+    DrawdownCurve,
+    PnlByTrade,
+    Scatter
+};
+
+enum class GraphAxis {
+
+};
+
+struct BacktestRequest {
+
+};
+
+struct BacktestTrade {
+
+};
+
+struct EquityPoint {
+
+};
+
+struct BacktestStats {
+
+};
+
+struct BacktestResult {
+
+};
+
+struct GraphRequest {
+
+};
+
+struct GraphPoint {
+
+};
